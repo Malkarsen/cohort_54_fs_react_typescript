5f2c68751c007e8e7eb00f34cb5afd09c75ba3a7
Malkarsen <malko.arina212@gmail.com>
2025-08-20 12:06:57 +0200

    Homework_07 Add type, Style in row

--- src/homeworks/Homework_07_Typescript, Parent_Child components/Homework_07.tsx
+++ src/homeworks/Homework_07_Typescript, Parent_Child components/Homework_07.tsx
@@ -7,21 +7,23 @@ function Homework_07() {
   const [like, setLike] = useState<number>(0);
   const [dislike, setDislike] = useState<number>(0);
 
-  const onLike = () => {
-    setLike((prevValue: number) => prevValue + 1);
+  const onLike = (): void => {
+    setLike((prevValue: number): number => prevValue + 1);
   };
 
-  const onDislike = () => {
-    setDislike((prevValue: number) => prevValue + 1);
+  const onDislike = (): void => {
+    setDislike((prevValue: number): number => prevValue + 1);
   };
 
-  const resetResults = () => {
+  const resetResults = (): void => {
     setLike(0);
     setDislike(0);
   };
 
+  const isRow = true;
+  
   return (
-    <div className="homework07_page_wrapper">
+    <div style={ {display: "flex", flexDirection: isRow ? "row" : "column"}} className="homework07_page_wrapper">
       <Feedback
         onLike={onLike}
         like={like}
